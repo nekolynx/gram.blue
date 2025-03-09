ae592ebea6db87646ff54bcf481ba5f085d81696
Pepi <pepi@he-five.com>
2025-03-09 11:44:11 -0400

    Cloudflare Integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -19,4 +21,8 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+
+export default nextConfig
