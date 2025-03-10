81f70a302043e51724b3dd70f5757cae7f01d5af
Pepi <pepi@he-five.com>
2025-03-10 14:59:53 -0400

    Squiggle Easter Egg

--- src/components/feedback/endOfFeed/EndOfFeed.tsx
+++ src/components/feedback/endOfFeed/EndOfFeed.tsx
@@ -1,7 +1,8 @@
 export default function EndOfFeed() {
   return (
-    <section className="border-b-skin-base md:border-x-skin-base flex flex-1 justify-center border-b p-3 md:rounded-b-2xl md:border-x">
-      <p className="text-skin-base">~ 🌸 Nothing more to show 🌸 ~</p>
+    <section className="border-b-skin-base md:border-x-skin-base flex flex-1 justify-center border-b p-3 md:rounded-b-2xl md:border-x relative overflow-hidden" style={{minHeight: "32px"}}>
+      <span className="text-xs bg-skin-base px-2 py-1 rounded-full">Nothing else here...</span>
+      <img src="/images/squiggle-x3.png" width="32px" className="absolute" style={{bottom: "-3px",right: "calc(50% - 96px)"}}/>
     </section>
   );
 }
