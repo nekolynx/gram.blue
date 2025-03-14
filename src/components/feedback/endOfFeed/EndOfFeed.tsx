6c353aceec877af0bff01ba08d0d0f33593004fb
Pepi <pepi@he-five.com>
2025-03-14 08:59:53 -0400

    Warning and weird css fix

--- src/components/feedback/endOfFeed/EndOfFeed.tsx
+++ src/components/feedback/endOfFeed/EndOfFeed.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
+
 export default function EndOfFeed() {
   return (
     <section className="border-b-skin-base md:border-x-skin-base flex flex-1 justify-center border-b p-3 md:rounded-b-2xl md:border-x relative overflow-hidden" style={{minHeight: "32px"}}>
       <span className="text-xs bg-skin-base px-2 py-1 rounded-full">Nothing else here...</span>
-      <img src="/images/squiggle-x3.png" width="32px" className="absolute" style={{bottom: "-3px",right: "calc(50% - 96px)"}}/>
+      <Image src="/images/squiggle-x3.png" width={32} height={32} className="absolute" alt="Squiggle Aligator easter egg" style={{bottom: "-3px",right: "calc(50% - 96px)"}}/>
     </section>
   );
 }
