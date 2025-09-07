2d417453abacdc31815aad60e59df7a0ce9b6032
Pepi <pepi16s@gmail.com>
2025-09-07 12:04:57 -0400

    Writing stupid code

--- src/components/feedback/sessionError/SessionError.tsx
+++ src/components/feedback/sessionError/SessionError.tsx
@@ -10,6 +10,13 @@ interface Props{
 export default function SessionError(props: Props) {
   const { message } = props;
 
+  if(message)
+  {
+    window.location.reload();
+    return null;
+  }
+  
+
   return (
     <div className="m-auto my-10 max-w-[20rem] shadow-xl shadow-status-danger/15 text-status-danger bg-skin-base flex flex-col items-center rounded-2xl px-7 py-8 font-medium text-center">
         <TbPlugConnectedX size={"2rem"}/>
@@ -17,7 +24,6 @@ export default function SessionError(props: Props) {
         <strong className="text-lg mb-0 mt-3 text-skin-base">Session Error</strong>
         <span className="text-skin-base">{message}</span>
         <Link href="../login" className="hover:cursor rounded-full bg-black/25 text-white px-2 py-2 pr-4 mt-6 flex items-center shadow-md shadow-black/10 font-bold"><IoRefreshCircle size="20pt"/>&nbsp;Re-Login</Link>
-        <Link href="." className="hover:cursor rounded-full bg-black/25 text-white px-2 py-2 pr-4 mt-6 flex items-center shadow-md shadow-black/10 font-bold"><IoRefreshCircle size="20pt"/>&nbsp;Reload App</Link>
     </div>
   );
 }
