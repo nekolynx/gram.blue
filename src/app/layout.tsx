ef41ddea6ec90aefc17aea2d55de8dff625610fc
Pepi <pepi16s@gmail.com>
2025-09-07 11:31:19 -0400

    Launch Screen for iPhone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,19 @@ export default async function RootLayout({
         {/* for making the page fullscreen on iOS when added to home */}
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-        <link rel="apple-touch-startup-image" href="/images/launch.png"></link>
+        <link rel="apple-touch-startup-image" href="/images/launch.png" />
+        <link rel="apple-touch-startup-image" href="/images/launch-(640x1136).png" media="(device-width: 640px) and (device-height: 1136px)"/>
+        <link rel="apple-touch-startup-image" href="/images/launch-(750x1334).png" media="(device-width: 750px) and (device-height: 1334px)"/>
+        <link rel="apple-touch-startup-image" href="/images/launch-(828x1792).png" media="(device-width: 828px) and (device-height: 1792px)"/>
+        <link rel="apple-touch-startup-image" href="/images/launch-(1125x2436).png" media="(device-width: 1125px) and (device-height: 2436px)"/>
+        <link rel="apple-touch-startup-image" href="/images/launch-(1170x2532).png" media="(device-width: 1170px) and (device-height: 2532px)"/>
+        <link rel="apple-touch-startup-image" href="/images/launch-(1179x2556).png" media="(device-width: 1179px) and (device-height: 2556px)"/>
+        <link rel="apple-touch-startup-image" href="/images/launch-(1206x2622).png" media="(device-width: 1206px) and (device-height: 2622px)"/>
+        <link rel="apple-touch-startup-image" href="/images/launch-(1242x2208).png" media="(device-width: 1242px) and (device-height: 2208px)"/>
+        <link rel="apple-touch-startup-image" href="/images/launch-(1242x2688).png" media="(device-width: 1242px) and (device-height: 2688px)"/>
+        <link rel="apple-touch-startup-image" href="/images/launch-(1284x2778).png" media="(device-width: 1284px) and (device-height: 2778px)"/>
+        <link rel="apple-touch-startup-image" href="/images/launch-(1290x2796).png" media="(device-width: 1290px) and (device-height: 2796px)"/>
+        <link rel="apple-touch-startup-image" href="/images/launch-(1320x2868).png" media="(device-width: 1320px) and (device-height: 2868px)"/>
       </head>
       <body className={inter.className}>
         <ThemeProvider>
